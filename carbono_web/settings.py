f7a5507ecd8065a327524c4adcefb8f98f562ab2
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2023-08-03 22:07:00 -0300

    validações login

--- carbono_web/settings.py
+++ carbono_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  # midia
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+#mensagens
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-primary',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
