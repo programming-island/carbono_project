fbc4d2bf994649bdb453807dd848bdc62b94769b
DaviFeitosa <davisfeitosa21@gmail.com>
2023-08-12 14:25:06 -0300

    criando mensagem de aviso com javascript

--- website/static/js/login.js
+++ website/static/js/login.js
@@ -1,3 +1,19 @@
-const mensagemError = document.querySelector("#usuario")
+document.getElementById("form-login").addEventListener("submit", function(evento) {
+    evento.preventDefault()
 
-console.log(mensagemError)
+    var usuario = document.getElementById("usuario").value
+    var senha = document.getElementById("senha").value
+
+    var msgUsuario = document.getElementById("msg-login")
+    var msgSenha = document.getElementById("msg-senha")
+
+    if(usuario === "") {
+        msgUsuario.style.display = "block"
+
+        setTimeout(function() {
+            msgUsuario.style.display = "none"
+        }, 10000) //tempo para sumir a mensagem
+    }
+    
+})
+
