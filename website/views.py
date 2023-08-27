dc785ef354c81cb2b9713b3f3b83748b851c9616
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2023-08-27 12:11:00 -0300

    colocando o views do cadastro de tipo de imovel

--- website/views.py
+++ website/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.core.validators import validate_email
-import re
 from django.contrib.auth import authenticate, login ,logout
-from django.contrib.auth.models import User
-from .models import usuarios,Imovel
+from .models import Imovel,TipoImovel
 
 
 # Create your views here.
 def home(request):
     imoveis = Imovel.objects.all()
-    context = {'imoveis': imoveis}
+    tipoimoveis = TipoImovel.objects.all()
+    context = {'imoveis': imoveis, 'tipoimoveis':tipoimoveis}
     return render(request, 'home.html',context)
 
 def imovel_detalhes(request,imovel_id):
